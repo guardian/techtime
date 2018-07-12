fb82e7f58f27e278a691db10ebdc9d9c5ee5325f
Mark Richards <mark.richards@guardian.co.uk>
2018-07-12 15:01:38 +0100

    Mark Richards: Brute Force

---
+++ Coding Challenges/20180712-Weekly/solutions/markrichards/index.js
@@ -0,0 +1,87 @@
+function canvasOnImg(img) {
+    var canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    let context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0, img.width, img.height);
+    return canvas
+}
+
+function countBlack(data) {
+    var blackCount = 0;
+    let pixelCount = (400 * 400);
+    for (var i = 0; i < pixelCount; i++) {
+        var offset = (i * 4);
+        if (data[offset] == 1 && data[offset + 1] == 1 && data[offset + 2] == 1) {
+            blackCount++
+        }
+    }
+    return blackCount
+}
+
+function drawCircle(context, centerX, centerY, radius) {
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.lineWidth = 1;
+    context.strokeStyle = '#f11';
+    context.stroke();
+
+}
+
+
+function forEachHtmlCollection(htmlCollection, func) {
+    for (var i = 0; i < htmlCollection.length; i++) {
+        var element = htmlCollection[i]
+        func(element)
+    }
+}
+
+function testCircle(x, y, img, testCanvas, testContext, circleSize, count) {
+    testContext.clearRect(0, 0, testCanvas.width, testCanvas.height);
+    testContext.drawImage(img, 0, 0, img.width, img.height);
+    drawCircle(testContext, x, y, circleSize)
+    let counted = countBlack(testContext.getImageData(0,0,400,400).data);
+    if (counted === count) {
+        return testCanvas
+    }
+    else {
+        testContext.restore()
+        return false;
+    }
+}
+
+window.onload = function () {
+    forEachHtmlCollection(document.getElementById("playground").getElementsByTagName('img'), img => {
+            var originalCanvas = canvasOnImg(img);
+            var originaldata = originalCanvas.getContext('2d').getImageData(0, 0, 400, 400).data
+            var testCanvas = canvasOnImg(img)
+
+            var testContext =testCanvas.getContext('2d')
+            testContext.save()
+            var count = countBlack(originaldata);
+            console.log(count)
+            var foundCanvas = false;
+            for (var i = 0; i < 200 && !foundCanvas; i++) {
+                console.log(i)
+                var circleSize = 200 - i;
+                for (var x = -i; x <= i && !foundCanvas; x++) {
+                    foundCanvas = testCircle(x+200, -i + 200,img, testCanvas, testContext,circleSize, count)
+                }
+                for (var x = -i; x <= i && !foundCanvas; x++) {
+                    foundCanvas = testCircle(x+200, i + 200,img, testCanvas,  testContext,circleSize, count)
+                }
+                for (var y = -i + 1; y < i && !foundCanvas; y++) {
+                    foundCanvas = testCircle(-i + 200, y+200, img, testCanvas, testContext,circleSize, count)
+                }
+                for (var y = -i + 1; y < i && !foundCanvas; y++) {
+                    foundCanvas = testCircle(i + 200, y+200, img, testCanvas, testContext,circleSize, count)
+                }
+            }
+            if (foundCanvas) {
+                img.src = foundCanvas.toDataURL()
+            }
+        }
+    )
+
+
+}
