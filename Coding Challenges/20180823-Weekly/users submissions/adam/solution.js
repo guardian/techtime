c5ef5e893fe674b2863eff7afeda97bb787693e7
Pascal Honoré <pascal@alseyn.net>
2018-08-29 13:58:13 +0100

    Pascal's comment

--- Coding Challenges/20180823-Weekly/users submissions/adam/solution.js
+++ Coding Challenges/20180823-Weekly/users submissions/adam/solution.js
@@ -1,3 +1,5 @@
 // create a var called input that contains the calculator string and run the following
 
 eval(input) + " 0 +"
+
+// Pascal's note: Adam making a point that I somehow never made it explicit that the terms were not to be evaluated/reduced, even though that was "obvious" (^_^)
