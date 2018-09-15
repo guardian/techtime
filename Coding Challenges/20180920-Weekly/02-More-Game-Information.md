3c8270c733d1594217c34cea4d80cfd3d3482e61
Pascal Honoré <pascal@alseyn.net>
2018-09-15 21:13:15 +0100

    Space Battle: Initial Commit

---
+++ Coding Challenges/20180920-Weekly/02-More-Game-Information.md
@@ -0,0 +1,7 @@
+## Additional game information
+
+This document is a complement to the introduction and contains more details about the game.
+
+- The game happens within a square of size **1000x1000**. ( The map's points have their 2D coordinates chosen between **0** and **1000** ).
+
+- Maps have **500** possible jump points.
---
+++ Coding Challenges/20180920-Weekly/game-server/web-server.rb
@@ -0,0 +1,125 @@
+
+# encoding: UTF-8
+
+# ------------------------------------------------------------
+# utils
+
+require 'sinatra'
+# http://www.sinatrarb.com/intro.html
+
+require 'securerandom'
+# SecureRandom.hex    #=> "eb693ec8252cd630102fd0d0fb7c3485"
+# SecureRandom.hex(2) #=> "eb69"
+# SecureRandom.uuid   #=> "2d931510-d99f-494a-8c67-87feb05e1594"
+
+require 'digest/sha1'
+# Digest::SHA1.hexdigest 'foo'
+# Digest::SHA1.file(myFile).hexdigest
+
+require 'json'
+
+require 'find'
+
+# --  --------------------------------------------------
+
+set :port, 14561
+#set :public_folder, "path/to/www"
+
+# -- --------------------------------------------------
+
+
+LUCILLE_INSTANCE = ENV["COMPUTERLUCILLENAME"]
+
+if LUCILLE_INSTANCE.nil? then
+    puts "Error: Environment variable 'COMPUTERLUCILLENAME' is not defined."
+    exit
+end
+
+DATA_FOLDER_PATH = "/Galaxy/DataBank/WeeklyCodingChallenges/20180920-Weekly/#{LUCILLE_INSTANCE}"
+
+class GameLibrary
+
+    # GameLibrary::hourCode()
+    def self.hourCode()
+        Time.new.strftime("%Y-%m-%d-%H")
+    end
+
+    # GameLibrary::getHoursFolderPaths()
+    def self.getHoursFolderPaths()
+        Dir.entries(DATA_FOLDER_PATH)
+            .select{|filename| filename[0,1]!="." }
+            .map{|filename| "#{DATA_FOLDER_PATH}/#{filename}" }
+    end
+
+    # GameLibrary::getFolderpathForThisHourCreateIfNotExists(): folderpath
+    def self.getFolderpathForThisHourCreateIfNotExists()
+        folderpath = "#{DATA_FOLDER_PATH}/#{GameLibrary::hourCode()}"
+        if !File.exists?(folderpath) then
+            FileUtils.mkpath folderpath
+        end
+        folderpath
+    end
+
+    # GameLibrary::getMapAtHourFolderCreateIfNotExists(folderpath)
+    def self.getMapAtHourFolderCreateIfNotExists(folderpath)
+        mapfilepath = "#{folderpath}/map.json"
+        if !File.exists?(mapfilepath) then
+            mapCardinality = 500 
+                # Math.sqrt(500) = 44.72 # number of jump points on a line. 
+                # 1000.to_f/44.72 = 22.36 # kilometers between jump points if in a square lattice. 
+            map = {}
+            map["mapId"] = SecureRandom.uuid
+            map["timestamp"] = GameLibrary::hourCode()
+            map["points"] = (1..mapCardinality).map{|indx|
+                {
+                    "label" => SecureRandom.hex(3),
+                    "coordinates" => [ rand * 1000, rand * 1000 ].map{|c| c.round(2) }
+                }
+            }
+            File.open(mapfilepath, "w"){ |f| f.puts(JSON.pretty_generate(map)) }
+        end
+        JSON.parse(IO.read(mapfilepath))
+    end
+
+    # GameLibrary::getCurrentMap()
+    def self.getCurrentMap()
+        folderpath = GameLibrary::getFolderpathForThisHourCreateIfNotExists()
+        GameLibrary::getMapAtHourFolderCreateIfNotExists(folderpath)
+    end
+
+    # GameLibrary::getPointForlabelAtMapOrNull(label, map)
+    def self.getPointForlabelAtMapOrNull(label, map)
+        map["points"].each{|point|
+            return point if point["label"]==label
+        }
+        nil
+    end
+
+    # GameLibrary::distanceBetweenTwoPoints(point1, point2)
+    def self.distanceBetweenTwoPoints(point1, point2)
+        dx = point1["coordinates"][0] - point2["coordinates"][0]
+        dy = point1["coordinates"][1] - point2["coordinates"][1]
+        Math.sqrt( (dx**2) + (dy**2) )
+    end
+
+end
+
+# -- --------------------------------------------------
+# Route
+
+not_found do
+  '404'
+end
+
+get '/' do
+    content_type 'text/plain'
+    [
+        "Space Battle. Game Server. Running at #{LUCILLE_INSTANCE}",
+        "See https://github.com/guardian/techtime/tree/master/Coding%20Challenges/20180920-Weekly for details."
+    ].join("\n")
+end
+
+get '/game/v1/map' do
+    content_type 'application/json'
+    JSON.pretty_generate(GameLibrary::getCurrentMap())
+end
